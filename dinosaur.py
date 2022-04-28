7bb933d589725e82e6fbde805bd94fbf2fa5d517
unknown <cheyenne.schwartz07@gmail.com>
2022-04-27 21:49:58 -0500

    Finished dinosaur file

---
+++ dinosaur.py
@@ -0,0 +1,14 @@
+
+class Dinosaur:
+
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.health = 100
+        self.attack_power = attack_power
+
+    def attack(self, robot):
+        robot.health = robot.health - self.attack_power
+        print(f'{robot.name} is down to {robot.health}!')
+        print('')
+
+
