bca33a61bf6eb22866f4ec50763aa1934a0178bd
unknown <cheyenne.schwartz07@gmail.com>
2022-04-27 21:51:58 -0500

    Finished robot file

---
+++ robot.py
@@ -0,0 +1,12 @@
+from weapon import Weapon
+
+
+class Robot:
+    def __init__ (self, name):
+        self.name = ()
+        self.health = 100
+        self.weapon = Weapon()
+
+    def attack(self, dinosaur):
+        dinosaur.health -= self.weapon.attack_power
+        print(f"{dinosaur.name} is down to {dinosaur.health}!")
