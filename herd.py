bf4c981e53361c46196a7403a529fdb079213cb4
unknown <cheyenne.schwartz07@gmail.com>
2022-04-27 21:51:21 -0500

    Finished herd file

---
+++ herd.py
@@ -0,0 +1,19 @@
+
+from dinosaur import Dinosaur
+
+
+class Herd:
+    def __init__(self):
+        self.dinosaurs = []
+        self.create_herd()
+
+    def create_herd(self):
+        dinosaur_one = Dinosaur('TRex', 70)
+        dinosaur_two = Dinosaur('Stegosaurus', 30)
+        dinosaur_three = Dinosaur('Raptor', 50)
+
+        self.dinosaurs.append(dinosaur_one)
+        self.dinosaurs.append(dinosaur_two)
+        self.dinosaurs.append(dinosaur_three)
+
+
