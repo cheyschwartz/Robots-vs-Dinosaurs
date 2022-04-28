3ff3e50d05a92d74a63b58143af4f684a67af97b
unknown <cheyenne.schwartz07@gmail.com>
2022-04-27 22:03:08 -0500

    I added values to the weapon class

--- robot.py
+++ robot.py
@@ -3,10 +3,11 @@ from weapon import Weapon
 
 class Robot:
     def __init__ (self, name):
-        self.name = ()
+        self.name = name
         self.health = 100
-        self.weapon = Weapon()
+        self.weapon = Weapon('Tomahawk', 40)
 
     def attack(self, dinosaur):
         dinosaur.health -= self.weapon.attack_power
         print(f"{dinosaur.name} is down to {dinosaur.health}!")
+        print('')
